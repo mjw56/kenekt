b7eaaa59920fba2dc26e39fb680be0d5c88bed79
Mike Wilcox <mwilcox56@gmail.com>
2017-03-05 20:44:17 -0500

    add firebase config

---
+++ firebase.js
@@ -0,0 +1,7 @@
+module.exports = {
+    api_key: process.env.KENEKT_FIREBASE_API_KEY,
+    auth_domain: process.env.KENEKT_FIREBASE_AUTH_DOMAIN,
+    database_url: process.env.KENEKT_FIREBASE_DATABASE_URL,
+    storage_bucket: process.env.KENEKT_FIREBASE_STORAGE_BUCKET,
+    messaging_sender_id: process.env.KENEKT_FIREBASE_MESSAGING_SENDER_ID
+}
